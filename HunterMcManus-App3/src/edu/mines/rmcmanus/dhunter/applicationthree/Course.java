c13a029e3ac59e14c1470a086f33447559148182
Ryan McManus <rmcmanus@mines.edu>
2013-11-25 12:22:40 -0700

    Long press can now delete courses.

--- HunterMcManus-App3/src/edu/mines/rmcmanus/dhunter/applicationthree/Course.java
+++ HunterMcManus-App3/src/edu/mines/rmcmanus/dhunter/applicationthree/Course.java
@@ -1,5 +1,13 @@
 package edu.mines.rmcmanus.dhunter.applicationthree;
 
 public class Course {
-
+	String objectID;
+	String courseName;
+	String courseLocation;
+	String courseTime;
+	
+	public Course(String courseName, String objectID) {
+		this.courseName = courseName;
+		this.objectID = objectID;
+	}
 }
